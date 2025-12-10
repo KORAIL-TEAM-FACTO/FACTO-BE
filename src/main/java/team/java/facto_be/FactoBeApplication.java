363c3239b9a05aaa7454978e65edc89226447c08
박주원 <qkrwndnjs7308@gmail.com>
2025-12-10 19:56:01 +0900

    nice

--- src/main/java/team/java/facto_be/FactoBeApplication.java
+++ src/main/java/team/java/facto_be/FactoBeApplication.java
@@ -2,7 +2,9 @@ package team.java.facto_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan("team.java.facto_be")
 @SpringBootApplication
 public class FactoBeApplication {
 
--- src/test/java/team/java/facto_be/FactoBeApplicationTests.java
+++ src/test/java/team/java/facto_be/FactoBeApplicationTests.java
@@ -1,9 +1,17 @@
 package team.java.facto_be;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=" +
+                "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+})
 class FactoBeApplicationTests {
 
     @Test
