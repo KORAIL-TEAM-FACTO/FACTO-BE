7cf7ba6a8843be92e5439dcf8ac0982638673fbd
박주원 <qkrwndnjs7308@gmail.com>
2025-12-10 16:54:33 +0900

    기본세팅

---
+++ src/main/java/team/java/facto_be/FactoBeApplication.java
@@ -0,0 +1,13 @@
+package team.java.facto_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/java/facto_be/FactoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package team.java.facto_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
