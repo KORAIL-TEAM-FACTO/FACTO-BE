21733c721384ba3f4da02de658050b02be385222
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-12-12 22:40:01 +0900

    refactor: XML 처리

--- src/main/java/team/java/facto_be/global/feign/config/FeignConfig.java
+++ src/main/java/team/java/facto_be/global/feign/config/FeignConfig.java
@@ -1,10 +1,17 @@
 package team.java.facto_be.global.feign.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import feign.Retryer;
+import feign.codec.Decoder;
 import feign.codec.ErrorDecoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import team.java.facto_be.global.feign.error.Custom5xxRetryer;
 import team.java.facto_be.global.feign.error.CustomErrorDecoder;
 
@@ -30,5 +37,21 @@ public class FeignConfig {
     @Bean
     public Retryer retryer() {
         return new Custom5xxRetryer();
+    }
+
+    /**
+     * XML 응답을 처리하기 위한 Feign Decoder 빈.
+     */
+    @Bean
+    public Decoder feignDecoder() {
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        MappingJackson2XmlHttpMessageConverter xmlConverter = new MappingJackson2XmlHttpMessageConverter(xmlMapper);
+
+        ObjectFactory<HttpMessageConverters> messageConverters = () ->
+            new HttpMessageConverters(xmlConverter);
+
+        return new SpringDecoder(messageConverters);
     }
 }
