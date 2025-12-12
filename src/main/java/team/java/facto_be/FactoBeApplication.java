473e6310231ab67ea7bf2bbd1440d530ac75b11e
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-12-12 22:39:19 +0900

    refactor: 어노테이션 추가

--- src/main/java/team/java/facto_be/FactoBeApplication.java
+++ src/main/java/team/java/facto_be/FactoBeApplication.java
@@ -3,7 +3,9 @@ package team.java.facto_be;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @ConfigurationPropertiesScan("team.java.facto_be")
 @SpringBootApplication
 public class FactoBeApplication {
