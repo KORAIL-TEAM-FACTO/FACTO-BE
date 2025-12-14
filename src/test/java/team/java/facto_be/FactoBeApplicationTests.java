0aa13fd5562e7bb53500b69e6ab2a956bd17d5bf
박주원 <qkrwndnjs7308@gmail.com>
2025-12-14 22:54:27 +0900

    정상화는 박주원

--- src/test/java/team/java/facto_be/FactoBeApplicationTests.java
+++ src/test/java/team/java/facto_be/FactoBeApplicationTests.java
@@ -1,17 +1,11 @@
 package team.java.facto_be;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-        "spring.autoconfigure.exclude=" +
-                "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-                "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
-                "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
-})
+@SpringBootTest
+@Disabled("DB 연결이 필요한 통합 테스트")
 class FactoBeApplicationTests {
 
     @Test
