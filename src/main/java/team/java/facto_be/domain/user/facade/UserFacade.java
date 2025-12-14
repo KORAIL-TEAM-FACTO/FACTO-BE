2a5e84bf8202e5cba8807b9db8531f9a23e82c99
박주원 <qkrwndnjs7308@gmail.com>
2025-12-14 16:29:58 +0900

    필요없는 거 다 제거

--- src/main/java/team/java/facto_be/domain/user/facade/UserFacade.java
+++ src/main/java/team/java/facto_be/domain/user/facade/UserFacade.java
@@ -1,4 +1,4 @@
-﻿package team.java.facto_be.domain.user.facade;
+package team.java.facto_be.domain.user.facade;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
--- src/main/java/team/java/facto_be/domain/welfare/dto/WelfareBookmarkResponse.java
+++
@@ -1,9 +0,0 @@
-package team.java.facto_be.domain.welfare.dto;
-
-import java.time.LocalDateTime;
-
-public record WelfareBookmarkResponse(
-        String servId,
-        String servNm,
-        LocalDateTime bookmarkedAt
-) { }
